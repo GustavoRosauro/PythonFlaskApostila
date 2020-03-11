c1eb1c3c449094ef61f87ffc91b3f5d488658ac3
GustavoRosauro <gustavosrosauro@gmail.com>
2020-03-11 12:15:10 -0300

    feitoInsert

--- AngularPython/src/app/apex/apex.component.ts
+++ AngularPython/src/app/apex/apex.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Aluno } from './aluno/aluno';
 
 @Component({
   selector: 'app-apex',
@@ -12,12 +13,21 @@ export class ApexComponent implements OnInit {
 
   ngOnInit() {
     this.carregaAlunos();
+    this.aluno = new Aluno();
   }
   private lista:any = [];
+  private aluno:Aluno;
+  private texto:string = 'Salvar'
   async carregaAlunos(): Promise<void>{
     await this.http.get('http://localhost:5000').subscribe(result =>{
       this.lista = result;
       console.log(result)
     }, error => console.log(error))
   }
+  async salvar():Promise<void>{
+    await this.http.post('http://localhost:5000',this.aluno).subscribe(result =>{
+      alert(result)
+      this.carregaAlunos();
+    }, error => console.log(error))
+  } 
 }
